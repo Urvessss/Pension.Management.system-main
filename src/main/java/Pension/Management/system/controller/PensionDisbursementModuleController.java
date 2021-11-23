9be3f2b72632d6fadc16f9b4521fc8b5bed83356
Urvesh <90395969+Urvessss@users.noreply.github.com>
2021-11-23 13:57:41 +0530

    Initial commit

---
+++ src/main/java/Pension/Management/system/controller/PensionDisbursementModuleController.java
@@ -0,0 +1,39 @@
+package Pension.Management.system.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import Pension.Management.system.service.PensionDisbursmentService;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+public class PensionDisbursementModuleController {
+
+	private static Logger logger = LoggerFactory.getLogger(PensionDisbursementModuleController.class);
+	@Autowired
+	private PensionDisbursmentService pensionDisbursementService;
+	@PostMapping("/DisbursePension")
+	@ApiOperation(value="Provides the processing code",
+					notes="10 - successful calculation of pension amount , 21 - wrong calculation of pension amount")
+	public int processPension(@RequestHeader("Authorization") String token,
+			@RequestBody ProcessPensionInput processPensionInput) {
+		logger.info("START");
+		int processCode = 21;
+		if (pensionDisbursementService.equals(processPensionInput)) {
+			processCode = pensionDisbursementService.processPension(token, processPensionInput);
+			logger.info("END");
+			return processCode;
+		}
+		logger.info("END - Token invalid");
+
+		return processCode;
+	}
+	
+	
+	
+}
---
+++ src/main/java/Pension/Management/system/service/PensionDisbursmentService.java
@@ -0,0 +1,47 @@
+package Pension.Management.system.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Pension.Management.system.model.Pensioner;
+import Pension.Management.system.model.PensionerDetails;
+
+@Service
+public class PensionDisbursmentService {
+	private static Logger logger = LoggerFactory.getLogger(PensionDisbursmentService.class);
+
+	@Autowired
+	PensionerDetails pensionerDetails;
+
+	public int processPension( ProcessPensionInput processPensionInput) {
+		logger.info("START");
+
+		Pensioner pensioner = PensionerDetails.getPensionerDetailByAadhaar(processPensionInput.getAadhaarNumber());
+		if (pensioner == null) {
+			logger.info("END");
+
+			return 21;
+		}
+
+		double bankServiceCharge = banks.get(pensioner.getBankName().toUpperCase());
+		if ((processPensionInput.getPensionAmount().equals(pensioner.getPensionAmount()))
+				&& (processPensionInput.getBankServiceCharge().equals(bankServiceCharge))) {
+			pensioner.setBankServiceCharge(bankServiceCharge);
+			if (pensionerDetailClient.logTransaction(token, pensioner)
+					&& pensionerDetailClient.updatePensioner(token, pensioner)) {
+				logger.info("END");
+
+				return 10;
+			}
+		}
+		logger.info("END");
+
+		return 21;
+	}
+
+}
